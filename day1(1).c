3a9f9dcc4dc617bff06eafa08b383183a95833b1
yashikakathait <yashikathait@gmail.com>
2026-08-10 20:45:29 +0530

    Update day1(1).c

--- day1(1).c
+++ day1(1).c
@@ -1,4 +1,4 @@
-//Q1 User inputs, operations, and outputs - Write a program to input two numbers and display their sum
+//Q1 Write a program to input two numbers and display their sum
 #include <stdio.h>
 int main()
 {int a; int b;
