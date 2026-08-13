8b20490041289187ed9cd331b33eb7cf2f80213a
yashikakathait <yashikathait@gmail.com>
2026-08-13 22:49:23 +0530

    Add files via upload

---
+++ day4_2.c
@@ -0,0 +1,10 @@
+// Write a program to find sum of n natural numbers
+#include <stdio.h>
+int main()
+{long n;
+    printf("Give positive integer value ");
+    scanf("%ld", &n);
+    long sum=(n*(n+1))/2;
+    printf("The sum of first %ld natural numbers is %ld", n, sum);
+    return 0;
+}
